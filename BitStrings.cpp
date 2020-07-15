3e487741332f2abac8fc11d0b34194eb2c019c44
warshmellow <ddwchensf@gmail.com>
2020-07-15 06:37:27 -0700

    bit strings

---
+++ BitStrings.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+typedef long long ll;
+
+ll f(ll n)
+{
+    ll mod = 1000000000 + 7;
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    ll result = 2;
+    for (ll i = 1; i < n; i++)
+    {
+        result = (result * 2) % mod;
+    }
+    return result;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+
+    ll result = f(n);
+
+    cout << result << "\n";
+}
