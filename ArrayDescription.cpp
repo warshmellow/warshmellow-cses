e3e5aa14811c0736bb5665068c1444b7fdf28920
david.chen <david.chen@iterable.com>
2020-08-02 14:46:19 -0700

    update ArrayDescription

--- ArrayDescription.cpp
+++ ArrayDescription.cpp
@@ -3,20 +3,35 @@
 
 using namespace std;
 
-int main() {
-  int MOD = 1000000000 + 7;
+int MOD = 1000000000 + 7;
 
+int f(vector<int> xs, int m) {
+  int n = xs.size();
+  if (n == 0) {
+    return 1;
+  } else if (n == 1 && xs[0] == 0) {
+    if (xs[0] == 0)
+      return m + 1;
+    else
+      return 1;
+  } else if (n == 2) {
+    return 0;
+  }
+  return 0;
+}
+
+int main() {
   int n;
   cin >> n;
   int m;
   cin >> m;
 
-  int xs[n];
+  vector<int> xs;
   for (int i = 0; i < n; i++) {
     int x;
     cin >> x;
-    xs[i] = x;
+    xs.push_back(x);
   }
 
-  cout << 3 % MOD << "\n";
+  cout << f(xs, m) % MOD << "\n";
 }
