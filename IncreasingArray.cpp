7f7a70198775c77d29c22707851cd68307d8b854
warshmellow <ddwchensf@gmail.com>
2021-08-16 09:37:24 -0400

    add IncreasingArray

--- IncreasingArray.cpp
+++ IncreasingArray.cpp
@@ -4,10 +4,40 @@
 
 using namespace std;
 
+typedef long long ll;
+
+ll g(vector<ll> nums) {
+  ll total = 0;
+  ll curr_max = nums[0];
+  size_t n = nums.size();
+
+  for (size_t i = 1; i < n; i++) {
+    ll curr = nums[i];
+    if (curr_max > curr) {
+      ll diff = curr_max - curr;
+      total += diff;
+    }
+    curr_max = max(curr_max, curr);
+  }
+  return total;
+}
+
+void solve() {
+  size_t n;
+  cin >> n;
+
+  vector<ll> nums;
+  for (size_t i = 0; i < n; i++) {
+    ll x;
+    cin >> x;
+    nums.push_back(x);
+  }
+  cout << g(nums) << "\n";
+}
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  int n;
-  cin >> n;
+  solve();
+  return 0;
 }
