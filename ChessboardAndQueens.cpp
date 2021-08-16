102f603ddcfd61e65f9995971a1cb6cb3375485a
warshmellow <ddwchensf@gmail.com>
2021-08-16 10:09:42 -0400

    typo

--- ChessboardAndQueens.cpp
+++ ChessboardAndQueens.cpp
@@ -14,14 +14,14 @@ vector<bool> diag2(2 * n, false);
 void search(size_t y, ll& count, vector<string> board) {
   if (y == n) {
     count += 1;
-    return;
-  }
-  for (size_t x = 0; x < n; x++) {
-    if (!(col[x] || diag1[x + y] || diag2[x - y + n - 1] ||
-          board[y][x] == '*')) {
-      col[x] = diag1[x + y] = diag2[x - y + n - 1] = true;
-      search(y + 1, count, board);
-      col[x] = diag1[x + y] = diag2[x - y + n - 1] = false;
+  } else {
+    for (size_t x = 0; x < n; x++) {
+      if (!(col[x] || diag1[x + y] || diag2[x - y + n - 1] ||
+            board[y][x] == '*')) {
+        col[x] = diag1[x + y] = diag2[x - y + n - 1] = true;
+        search(y + 1, count, board);
+        col[x] = diag1[x + y] = diag2[x - y + n - 1] = false;
+      }
     }
   }
 }
