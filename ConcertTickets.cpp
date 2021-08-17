171a1b5939e9c5dfbfc579eb54330ff62c73aaac
warshmellow <ddwchensf@gmail.com>
2021-08-17 10:17:27 -0400

    concert ticket

--- ConcertTickets.cpp
+++ ConcertTickets.cpp
@@ -1,24 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
 
 using namespace std;
 
-// variables used for the current problem
-int n, m, h, t;
-multiset<int> tickets;
-
 void solve() {
+  int n, m, h, t;
+  multiset<int> tickets;
   cin >> n >> m;
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; i++) {
     cin >> h;
     tickets.insert(h);
   }
-  for (int i = 0; i < m; ++i) {
+  for (int i = 0; i < m; i++) {
     cin >> t;
     auto it = tickets.upper_bound(t);
     if (it == tickets.begin()) {
       cout << -1 << "\n";
     } else {
-      cout << *(--it) << "\n";
+      it--;
+      cout << *it << "\n";
       tickets.erase(it);
     }
   }
