7aaf2fc5a83b2bd46fc4b12f7fa66dc55cce20df
warshmellow <ddwchensf@gmail.com>
2021-08-17 10:54:06 -0400

    maxsubarraysum

--- MaximumSubarraySum.cpp
+++ MaximumSubarraySum.cpp
@@ -6,30 +6,32 @@ typedef long long ll;
 
 using namespace std;
 
-ll f(ll nums[], int n) {
+void solve() {
+  size_t n;
+  cin >> n;
+
+  vector<ll> nums;
+  for (size_t i = 0; i < n; i++) {
+    ll num;
+    cin >> num;
+    nums.push_back(num);
+  }
+
   ll max_so_far = nums[0];
   ll max_curr = nums[0];
 
-  for (int i = 1; i < n; i++) {
+  for (size_t i = 1; i < n; i++) {
     ll curr = nums[i];
     max_curr = max(max_curr + curr, curr);
     max_so_far = max(max_so_far, max_curr);
   }
-  return max_so_far;
+
+  cout << max_so_far << "\n";
 }
 
 int main() {
-  int n;
-  cin >> n;
-
-  ll nums[n];
-  for (int i = 0; i < n; i++) {
-    ll num;
-    cin >> num;
-    nums[i] = num;
-  }
-
-  ll result = f(nums, n);
-
-  cout << result << "\n";
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+  return 0;
 }
