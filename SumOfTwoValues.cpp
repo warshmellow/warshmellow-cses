75f587128713716149f8c066da34be90d78b8e34
warshmellow <ddwchensf@gmail.com>
2021-08-17 10:40:25 -0400

    update sum of two values

--- SumOfTwoValues.cpp
+++ SumOfTwoValues.cpp
@@ -1,42 +1,66 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
 #include <vector>
 
 using namespace std;
 
-pair<int, int> g(vector<int> a, int x) {
-  int n = a.size();
+typedef long long ll;
 
-  for (int i = 0; i < n; i++) {
-    for (int j = i + 1; j < n; j++) {
-      if (a[i] + a[j] == x) {
-        return {i + 1, j + 1};
+pair<ll, ll> g(vector<ll> a, ll x) {
+  size_t n = a.size();
+
+  vector<pair<ll, size_t>> a_with_idx;
+  for (size_t i = 0; i < n; i++) {
+    a_with_idx.push_back(make_pair(a[i], i));
+  }
+  sort(a_with_idx.begin(), a_with_idx.end());
+
+  for (pair<ll, size_t> p : a_with_idx) {
+    ll y = p.first;
+    size_t i = p.second;
+
+    ll diff = x - y;
+
+    auto it = lower_bound(a_with_idx.begin(), a_with_idx.end(),
+                          make_pair(diff, n + 1));
+    size_t j = it - a_with_idx.begin();
+    if (j > 0) {
+      pair<ll, size_t> pcand = a_with_idx[j - 1];
+      ll cand = pcand.first;
+      size_t cand_idx = pcand.second;
+      if (cand == diff && cand_idx > i) {
+        return make_pair(i + 1, cand_idx + 1);
       }
     }
   }
-  return {-1, -1};
+
+  return make_pair(-1, -1);
 }
 
-int main() {
-  int n;
-  cin >> n;
-  int x;
-  cin >> x;
+void solve() {
+  size_t n;
+  ll x;
+  vector<ll> a;
 
-  vector<int> a;
-  for (int i = 0; i < n; i++) {
-    int y;
+  cin >> n >> x;
+
+  for (size_t i = 0; i < n; i++) {
+    ll y;
     cin >> y;
     a.push_back(y);
   }
 
-  pair<int, int> result = g(a, x);
-
-  if (result.first == -1) {
-    cout << "IMPOSSIBLE"
-         << "\n";
+  pair<ll, ll> p = g(a, x);
+  if (p.first == -1) {
+    cout << "IMPOSSIBLE\n";
   } else {
-    cout << result.first << " " << result.second << "\n";
+    cout << p.first << " " << p.second << "\n";
   }
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+  return 0;
 }
