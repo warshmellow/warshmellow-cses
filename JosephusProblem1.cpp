3f1346f763c3560dfedc319f67f580158fb87a20
warshmellow <ddwchensf@gmail.com>
2021-08-18 07:30:55 -0400

    touchup

--- JosephusProblem1.cpp
+++ JosephusProblem1.cpp
@@ -6,8 +6,9 @@ typedef long long ll;
 
 using namespace std;
 
-vector<size_t> josh(vector<size_t> person, size_t k, size_t index) {
+vector<size_t> josh(vector<size_t> person, size_t k) {
   vector<size_t> result;
+  size_t index = 0;
   while (person.size() > 1) {
     index = (index + k) % person.size();
     result.push_back(person[index]);
@@ -23,16 +24,13 @@ void solve() {
 
   size_t k = 2;
   k--;
-  size_t index = 0;
 
   vector<size_t> person;
   for (size_t i = 1; i < n + 1; i++) {
     person.push_back(i);
   }
 
-  vector<size_t> result = josh(person, k, index);
-
-  for (size_t x : result) {
+  for (size_t x : josh(person, k)) {
     cout << x << " ";
   }
   cout << "\n";
