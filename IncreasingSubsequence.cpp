22294bd2d5ac092c7e329e163d4f29aa0dcbb1ee
warshmellow <ddwchensf@gmail.com>
2021-08-23 09:48:07 -0400

    passing IncreasingSubsequence

--- IncreasingSubsequence.cpp
+++ IncreasingSubsequence.cpp
@@ -4,33 +4,28 @@
 
 using namespace std;
 
-int main() {
+void solve() {
   int n;
   cin >> n;
+  vector<int> dp;
 
-  int nums[n];
   for (int i = 0; i < n; i++) {
-    int num;
-    cin >> num;
-    nums[i] = num;
-  }
-
-  int dp[n];
-  // dp[i] = longest increasing subequence ending at and including nums[i]
-  // dp[0] = 1
-  // max of dp is result
-  dp[0] = 1;
-  for (int i = 1; i < n; i++) {
-    dp[i] = 1;
-
-    for (int j = 0; j < i; j++) {
-      if (nums[j] < nums[i]) {
-        dp[i] = max(dp[i], dp[j] + 1);
-      }
+    int x;
+    cin >> x;
+    auto it = lower_bound(dp.begin(), dp.end(), x);
+    if (it == dp.end()) {
+      dp.push_back(x);
+    } else {
+      *it = x;
     }
   }
 
-  int result = *max_element(dp, dp + n);
+  cout << dp.size() << "\n";
+}
 
-  cout << result << "\n";
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+  return 0;
 }
