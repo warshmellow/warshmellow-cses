f481719665e1a5b53982574748d11bedb680a79c
david.chen <david.chen@iterable.com>
2022-03-08 20:29:25 -0800

    add comments

--- subarray_divisibility.py
+++ subarray_divisibility.py
@@ -1,23 +1,32 @@
 def f(xs):
     n = len(xs)
 
-    cnt = 0
-    pre = 0
+    count = 0
+    prefix_sum = 0
 
-    freq = {}
-    freq[0] = 1
+    freq = {}  # frequency of seeing prefix sum % n
+    freq[0] = 1  # This allows for prefix sums to be counted
 
     for x in xs:
-        pre = (pre + x) % n
+        # Tracks prefix xs[:i + 1] % n
+        prefix_sum = (prefix_sum + x) % n
 
-        if pre in freq:
-            cnt += freq[pre]
+        if prefix_sum in freq:
+            """
+            Let 0 < j < i + 1.
+            If xs[:j] % n == xs[:i + 1] % n, then 
+            (xs[:i + 1] - xs[:j]) % n == xs[j: i + 1] % n == 0.
+
+            Thus every prefix not the same to current one but
+            with the same subarray sum % n has a difference a new subarray with sum 0 % n.
+            """
+            count += freq[prefix_sum]
         else:
-            freq[pre] = 0
+            freq[prefix_sum] = 0
 
-        freq[pre] += 1
+        freq[prefix_sum] += 1
 
-    return cnt
+    return count
 
 
 def main():
