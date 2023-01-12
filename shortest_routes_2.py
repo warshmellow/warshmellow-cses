acdca7aa82cbae15be9b6b0035ac0070bd1e65be
warshmellow <ddwchensf@gmail.com>
2023-01-12 10:09:56 -0500

    minor optimizations

--- shortest_routes_2.py
+++ shortest_routes_2.py
@@ -5,17 +5,12 @@ def main():
     MAX_DIST = math.inf
     n, m, q = [int(x) for x in input().split()]
 
-    # account for bug in Test 13
-    if (n, m, q) == (2, 2, 1):
-        print(1)
-        return
-
     dist = [[MAX_DIST for _ in range(n + 1)] for _ in range(n + 1)]
 
     for _ in range(m):
         a, b, c = [int(x) for x in input().split()]
-        dist[a][b] = c
-        dist[b][a] = c
+        dist[a][b] = min(dist[a][b], c)
+        dist[b][a] = min(dist[a][b], c)
 
     for v in range(1, n + 1):
         dist[v][v] = 0
@@ -23,8 +18,7 @@ def main():
     for k in range(1, n + 1):
         for i in range(1, n + 1):
             for j in range(1, n + 1):
-                if dist[i][j] > dist[i][k] + dist[k][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
     for _ in range(q):
         a, b = [int(x) for x in input().split()]
