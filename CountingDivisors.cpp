274ae15ac11523b53ca08bb01b977c06db333e52
warshmellow <ddwchensf@gmail.com>
2023-01-15 14:20:14 -0500

    add coutingDivisors.cpp

---
+++ CountingDivisors.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+vi g(int n) {
+  vi result(n);
+  for (int i = 1; i <= n; i++) {
+    for (int j = i; j <= n; j += i) {
+      result[j]++;
+    }
+  }
+  return result;
+}
+
+int main() {
+  int MAX_X = 1e6;
+
+  int n;
+  cin >> n;
+
+  vi result = g(MAX_X + 1);
+
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+
+    cout << result[x] << "\n";
+  }
+}
